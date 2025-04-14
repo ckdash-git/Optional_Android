d80a946352b50a8bbb1993b2520ac6e55fa9e340
Chandan Swain <134497712+chandan-swain@users.noreply.github.com>
2025-04-14 19:42:37 +0530

    Create main_screen.dart
    This is the controller for navigation bar

---
+++ lib/main_screen.dart
@@ -0,0 +1,44 @@
+// it will manage bottom navigation
+
+import 'package:flutter/material.dart';
+import 'package:optional/view2.dart';
+import 'package:optional/view4.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0; // Changed from 2 to 0 to show home screen by default
+
+  final List<Widget> _screens = const [
+    CustomUIScreen(), // Home
+    Placeholder(), // Search screen
+    ProfileScreen(), // Profile
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+      ),
+    );
+  }
+}
